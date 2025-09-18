9b68c7e0876ee800440fa40218e3e3fa14018029
Ishtiaq Ahmed <ishtiaqahmedz@hotmail.com>
2025-09-19 00:47:30 +0500

    data validation is completed

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import os
 from networksecurity.constant import training_pipeline
 
-print(training_pipeline.PIPELINE_NAME)
-print(training_pipeline.ARTIFACT_DIR)
+#print(training_pipeline.PIPELINE_NAME)
+#print(training_pipeline.ARTIFACT_DIR)
 
 
 class TrainingPipelineConfig:
