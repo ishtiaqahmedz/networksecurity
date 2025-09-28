e31c354d164db36090c5dc06f27b9e51a4ca5af9
Ishtiaq Ahmed <ishtiaqahmedz@hotmail.com>
2025-09-28 20:17:36 +0500

    completed till model trainer component

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -87,9 +87,8 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            model.fit(X_train,y_train) #bi beed ti train the model again as model.best_params_ will provide us the best model)
 
-            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
