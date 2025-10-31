50e69b4a11aecbae79056dbe68810f0bff6f5b19
Ishtiaq Ahmed <ishtiaqahmedz@hotmail.com>
2025-10-31 23:17:36 +0500

    after updting first change in .github/workflows/main.yaml

--- app.py
+++ app.py
@@ -84,5 +84,7 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
             raise NetworkSecurityException(e,sys)
 
     
-if __name__=="__main__":
+if __name__=="__main__":  #app_run is imported from uvicorn, it will run the app, using uvicorn server, that is why Dockers file has
+                            # the app command in the end of the file
+
     app_run(app,host="0.0.0.0",port=8000)
